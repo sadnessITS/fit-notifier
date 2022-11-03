7da92639402f82a3cb6579e1048293b7c9e73a2e
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-11-03 20:59:31 +0300

    dispatcher and logging added

--- kernel/kernel.py
+++ kernel/kernel.py
@@ -1,4 +1,9 @@
+import logging
 from aiogram import Dispatcher,  Bot
+from config import API_TOKEN
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 disp = Dispatcher(bot, storage=MemoryStorage())
