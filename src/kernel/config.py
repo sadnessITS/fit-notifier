640d6383e3ddcd55b6ef1726d0653d0d33c9452b
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-11-14 18:56:31 +0300

    git config updated added several constants for the database

--- src/kernel/config.py
+++ src/kernel/config.py
@@ -3,3 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv("./env/.env")
 API_TOKEN = os.getenv("API_TOKEN")
+SERVER = int(os.getenv("SERVER", 0))
+DB_NAME = os.getenv("DB_NAME", 'postgres')
+USER = os.getenv("USER", 'postgres')
+PASSWORD = os.getenv("PASSWORD", '')
+HOST = os.getenv("HOST", 'localhost')
+PORT = int(os.getenv("PORT", 5432))
