e445fd28fc707c3685a367cac9134603d2e9b5b9
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-11-18 15:21:05 +0300

    config and enc.example fixed

--- src/kernel/config.py
+++ src/kernel/config.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv("./env/.env")
+load_dotenv('env/.env', override=True)
+
 API_TOKEN = os.getenv("API_TOKEN")
 SERVER = int(os.getenv("SERVER", 0))
 DB_NAME = os.getenv("DB_NAME", 'postgres')
-USER = os.getenv("USER", 'postgres')
+USERNAME = os.getenv("USERNAME", 'postgres')
 PASSWORD = os.getenv("PASSWORD", '')
 HOST = os.getenv("HOST", 'localhost')
 PORT = int(os.getenv("PORT", 5432))
