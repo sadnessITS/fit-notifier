38336e52ae0592f1e3c9f662c39ebc0b5534d73e
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-11-18 15:23:48 +0300

    dbapi init file redesigned, Studend model added

--- src/dbapi/__init__.py
+++ src/dbapi/__init__.py
@@ -1,44 +1,35 @@
-import enum
-from exceptions import WrongDBNameException
-from peewee import PostgresqlDatabase, MySQLDatabase
+from kernel.config import *
+from dbapi.available_dbnames import AvailableDBs
+from peewee import *
+from dbapi.exceptions.wrong_dbname import WrongDBNameException
+
+match(SERVER):
+    case AvailableDBs.postgresql:
+        db = PostgresqlDatabase(
+            DB_NAME, user=USERNAME, password=PASSWORD,
+            host=HOST, port=PORT)
+    case AvailableDBs.mysql:
+        db = MySQLDatabase(
+            DB_NAME, user=USERNAME, password=PASSWORD,
+            host=HOST, port=PORT)
+    case _:
+        raise WrongDBNameException()
 
 
-class DBName(enum.IntEnum):
-    postgresql = 0,
-    mysql = 1,
+class Student(Model):
+    student_id = IntegerField(primary_key=True)
+    name = TextField(null=False)
+    surname = TextField(null=False)
+    patronymic = TextField(null=False)
+    course = SmallIntegerField(null=False)
+    group = SmallIntegerField(null=False)
+    is_budget = BooleanField(null=False)
+    is_expelled = BooleanField(null=False, default=False)
+    telegram_id = BigIntegerField(null=False)
+
+    class Meta:
+        database = db
 
 
-class DataBaseAPI:
-    def __init__(self, database: int, db_file_name: str, user: str,
-                 password: str, host: str, port: int):
-        '''This is where the parameters to connect the database are passed
-            @param database -- server, specify with enum DBName
-            @param db_file_name -- database name
-            @param user -- username
-            @param password -- password for user
-            @param host -- host
-            @param port -- port
-        '''
-        self.database: int = database
-        self.db_file_name: str = db_file_name
-        self.user: str = user
-        self.password: str = password
-        self.host: str = host
-        self.port: int = port
-
-    def get_instanse(self):
-        '''Get instance of selected database'''
-        match(self.database):
-            case DBName.postgresql:
-                self.db = PostgresqlDatabase(
-                    self.db_file_name, user=self.user, password=self.password,
-                    host=self.host, port=self.port)
-
-            case DBName.mysql:
-                self.db = MySQLDatabase(self.db_file_name, user=self.user, password=self.password,
-                                        host=self.host, port=self.port)
-
-            case _:
-                raise WrongDBNameException()
-
-        return self.db
+db.connect()
+db.create_tables([Student])
--- src/dbapi/exceptions/__init__.py
+++
@@ -1,3 +0,0 @@
-class WrongDBNameException(Exception):
-    def __init__(self, message="Wrong Database Name") -> None:
-        super().__init__(message)
